626d5912924ec342c6f185b958979101c8298f26
jologar <jologar.dev@gmail.com>
2021-09-06 20:29:11 +0200

    feature: basic gui implemented

--- gameui/tkinterui.py
+++ gameui/tkinterui.py
@@ -2,7 +2,13 @@ from gameui.ui import Ui
 from tkinter import *
 from tkinter import ttk
 from ai import GameAi
-from game import Game
+from game import Game, GameState, Player
+from PIL import ImageTk, Image as PImage
+import math
+import threading
+
+SQUARE_PADDING = 5
+SEPARATOR_WIDTH = 2
 
 class TkinterGui(Tk, Ui):
     def __init__(self, game: Game, ai: GameAi):
@@ -11,6 +17,11 @@ class TkinterGui(Tk, Ui):
         self.title("Tic Tac Toe")
         self.minsize(500, 400)
         self.maxsize(500, 400)
+        self.__game = game
+        self.__ai = ai
+        self.tiles = []
+        self.vline_separation = 0
+        self.hline_separation = 0
 
         mainframe = ttk.Frame(self, padding="3")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -21,39 +32,143 @@ class TkinterGui(Tk, Ui):
         self.rowconfigure(0, weight=1)
 
         self.__canvas = Canvas(mainframe, bg='white')
-        self.__canvas.bind("<Configure>", self.__build_board)
         self.__canvas.grid(row=0, column=0, sticky=(N, E, S, W))
-        
-        self.__buildMenu()
-        
     
-    def __buildMenu(self):
+        # Load images
+        self.__x_image = PImage.open("gameui/assets/x.jpeg")
+        self.__o_image = PImage.open("gameui/assets/o.jpg")
+        
+        # self.__game.set_human(Player.PLAYER_1)
+
+        self.__build_menu()
+        
+    def __bind_click(self):
+        self.__canvas.bind('<Button-1>', self.__on_click)
+
+    def __unbind_click(self):
+        self.__canvas.unbind('<Button-1>')
+
+    def __build_menu(self):
         menubar = Menu(self)
         menu_game = Menu(menubar)
         menubar.add_cascade(menu=menu_game, label='Game')
-        menu_game.add_command(label="New Game", command=self.__start_new_game)
+        menu_game.add_command(label="New Game", command=self.__start_game)
         # TODO: build the menu
-        
         self.config(menu=menubar)
-    
-    def __start_new_game(self):
-        print('Start new game!')
 
-    def __build_board(self, event: Event):
-        vseparation = event.width / 3
-        hseparation = event.height / 3
+    def __on_game_state_change(self):
+        self.__build_board(self.__canvas.winfo_width(), self.__canvas.winfo_height())
+        # Check if game is finished
+        game_state = self.__game.game_state()
+        if not game_state == GameState.ONGOING:
+            self.__endgame_actions(game_state)
+            self.__unbind_click()
+        elif not self.__game.is_human(self.__game.get_player_turn()):
+            # AI thread
+            self.ai_thread = threading.Thread(target=self.__ai_move, daemon=True)
+            self.__unbind_click()
+            self.ai_thread.start()
+            # self.ai_thread.join()
+            self.__bind_click()
+        else:
+            self.__unbind_click()
+            self.__bind_click()
+
+    def __endgame_actions(self, game_state: GameState):
+        if game_state == GameState.DRAW:
+            print('DRAW')
+        if game_state == GameState.VICTORY_O:
+            print('O WINS')
+        if game_state == GameState.VICTORY_X:
+            print('X WINS')
+        self.stop()
+        
+
+    def __start_game(self):
+        print('start')
+        # subscribe to game events
+        self.__game.subscribe(self.__on_game_state_change)
+        self.__game.reset_game()
+
+    def __ai_move(self):
+        print('ai turn')
+        player = self.__game.get_player_turn()
+        move = self.__ai.getPlayerMove(player, self.__game.get_game_data())
+        col = move % 3
+        row = int((move - col) / 3)
+        self.__game.do_move(row, col)
+        print('move done')
+
+    def __on_click(self, event):
+        player = self.__game.get_player_turn()
+        if self.__game.is_human(player):
+            move = self.__get_click_tile(event.x, event.y)
+            self.__game.do_move(move[0], move[1])
+
+    def __get_click_tile(self ,click_x: int, click_y: int):
+        # Get x tile dimension
+        if click_x < self.vline_separation:
+            col = 0
+        elif (self.vline_separation < click_x) and (click_x < 2 * self.vline_separation):
+            col = 1
+        else:
+            col = 2
+        # get y tile dimension
+        if click_y < self.hline_separation:
+            row = 0
+        elif (self.hline_separation < click_y) and (click_y < 2 * self.hline_separation):
+            row = 1
+        else:
+            row = 2
+        return (row, col)    
+
+    def __build_board(self, width: int, height: int):
+        vseparation = width / 3
+        hseparation = height / 3
         # vertical lines
-        self.__canvas.create_line(vseparation, 0, vseparation, event.height, width=4)
-        self.__canvas.create_line(2*vseparation, 0, 2*vseparation, event.height, width=4)
+        self.__canvas.create_line(vseparation, 0, vseparation, height, width=SEPARATOR_WIDTH)
+        self.__canvas.create_line(2*vseparation, 0, 2*vseparation, height, width=SEPARATOR_WIDTH)
         # horizontal lines
-        self.__canvas.create_line(0, hseparation, event.width, hseparation, width=4)
-        self.__canvas.create_line(0, 2*hseparation, event.width, 2*hseparation, width=4)
+        self.__canvas.create_line(0, hseparation, width, hseparation, width=SEPARATOR_WIDTH)
+        self.__canvas.create_line(0, 2*hseparation, width, 2*hseparation, width=SEPARATOR_WIDTH)
 
+        self.vline_separation = vseparation
+        self.hline_separation = hseparation
+        self.__draw_state(vseparation, hseparation)
+
+    def __build_board_event(self, event: Event):
+        self.__build_board(event.width, event.height)
+        
+    def __draw_state(self, square_width: int, square_height: int): 
+        # Draw current board state
+        state = self.__game.get_game_data()
+        # Init tiles
+        self.tiles = []
+        for idx in range(len(state)):
+            self.tiles.append(None)
+        width = math.floor(square_width - SQUARE_PADDING)
+        height =  math.floor(square_height - SQUARE_PADDING)
+        for idx, tile in enumerate(state):
+            if tile == Player.PLAYER_1.value:
+                self.tiles[idx] = ImageTk.PhotoImage(self.__o_image.resize((width, height)))
+            elif tile == Player.PLAYER_2.value:
+                self.tiles[idx] = ImageTk.PhotoImage(self.__x_image.resize((width, height)))
+            else:
+                self.tiles[idx] = None
+                continue
+        for i in range(3):
+            for j in range(3):
+                img = self.tiles[i*3 + j]
+                if tile != None:
+                    posX = math.floor(j * square_width + square_width / 2)
+                    posY = math.floor(i * square_height + square_height / 2)
+                    self.__canvas.create_image(posX, posY, image=img)
 
     def start(self):
         self.mainloop()
     
-    def stop():
-        pass
+    def stop(self):
+        self.__game.unsuscribe(self.__on_game_state_change)
+        
     def update():
         pass
